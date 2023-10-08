e51e140230cff1e64e4f4c199f73247ce176ca09
supriyogarai <146556008+supriyogarai@users.noreply.github.com>
2023-10-08 09:02:48 +0530

    Create vowel.c
    to check a character is vowel or not

---
+++ vowel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  char ch;
+  clrscr();
+  printf(\n enter any character= \n);
+  scanf("%c",&ch);
+  if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u');
+  printf("chararcter is vowel");
+  getch();
+}
